892cf939b1120a8526d096543ee704379b579a4f
Aman Kumar <kramansingh23@gmail.com>
2024-05-11 17:24:20 +0530

    init

---
+++ Programmes/Java_Programms/CoreJava/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+       int c = a + b;
+        System.out.println("sum of " +a+ " and "+b+" is "+ c);
+    }
+}
