716a8b2b9a9fafd805c69d9bc080ed7d1767ccd2
Aman Kumar <kramansingh23@gmail.com>
2024-05-12 17:09:50 +0530

    init

--- Programmes/Java_Programms/CoreJava/userInput.java
+++ Programmes/Java_Programms/CoreJava/userInput.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 class Userinput {
 
+    
     public static void main(String[] args) {
         System.out.println("Enter the Two NUmber for adding");
         try (Scanner sc = new Scanner(System.in)) {
